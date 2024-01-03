bdec0acb2887da36a8f8d6e1233a5e8b407ad187
garenaxviettel <hkvbestdz@gmail.com>
2024-01-03 11:52:29 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,5 +138,5 @@ function animatePageTurn() {
     // Thêm sự kiện click vào văn bản
     myText.addEventListener("click", function() {
         // Hiển thị thông báo
-        alert("Thật đó huhu🥹");
+        alert("Thật đó huhu🥲");
     });
